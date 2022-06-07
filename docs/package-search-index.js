00d939420318bbf1db1e75ebe743506fc5fe335d
Laurent SIMON <laurent.simon@stratic.fr>
2022-06-07 15:28:23 +0200

    Removed com.stratic.tpl.accessors package from the Javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.stratic.tpl"},{"l":"com.stratic.tpl.accessors"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.stratic.tpl"}];updateSearchResults();
